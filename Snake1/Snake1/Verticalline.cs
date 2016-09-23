0ae35edc87282875143dc5cec35ef314dcd0491e
alex <overjke12@gmail.com>
2016-09-24 00:16:41 +0400

    Линии из символов

---
+++ Snake1/Snake1/Verticalline.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class Verticalline
+    {
+        List<Point> pList;
+
+        public Verticalline (int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach (Point p in pList) 
+            {
+                p.Draw();
+            }
+        }
+    }
+}
